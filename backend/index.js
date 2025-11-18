7310eb0dd977b46e4428406a71c4d14bfcfcc7e4
laddu344 <valmiki.9398@gmail.com>
2025-11-18 16:39:19 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -56,7 +56,7 @@ app.get('/api/health', (req, res) => {
 const frontendPath = path.join(__dirname, 'frontend', 'dist');
 app.use(express.static(frontendPath));
 
-app.get('*', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(frontendPath, 'index.html'));
 });
 
